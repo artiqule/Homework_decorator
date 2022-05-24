59e0b50d53267d8e12e87fb882ba8499f698c59f
artiqule <94694652+artiqule@users.noreply.github.com>
2022-05-24 20:37:24 +0300

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 
 def decor_logger(old_func):
-    def foo(*args, **qwargs):
-        res = old_func(*args, **qwargs)
+    def foo(*args, ** qwargs):
+        res = old_func(*args, ** qwargs)
         with open('log_file.txt', 'w', encoding='utf-8') as file:
             file.write(
                 f'Дата и время вызова: {datetime.now()}\n'
